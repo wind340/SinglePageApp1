397003b8cfe6a5a9216880a714338ec78ae1e179
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-17 23:57:10 +0900

    default setting

---
+++ src/component/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <header></header>
+      <body></body>
+      <footer></footer>
+    </>
+  );
+};
+
+export default Home;
