de1ea204d56f0a40cb1a5ebd2722d9afc743a2e4
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-26 02:36:44 +0900

    _blank 보안취약 에러로 rel 추가

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {SiNotion} from 'react-icons/si'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="https://www.notion.so/jakedevchek/" target="_blank"><SiNotion/></a>
-      <a href="https://github.com/wind340/" target="_blank"><BsGithub/></a>
-      <a href="https://www.instagram.com/a_jc.k/" target="_blank"><BsInstagram/></a>
+      <a href="https://www.notion.so/jakedevchek/" target="_blank" rel="noreferrer"><SiNotion/></a>
+      <a href="https://github.com/wind340/" target="_blank" rel="noreferrer"><BsGithub/></a>
+      <a href="https://www.instagram.com/a_jc.k/" target="_blank" rel="noreferrer"><BsInstagram/></a>
     </div>
   )
 }
