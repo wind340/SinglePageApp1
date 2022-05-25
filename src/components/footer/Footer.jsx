5f25eb17383bde7601bdfc49c3b9ad43dd448927
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-26 03:37:31 +0900

    footer page 작성

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 import './footer.css'
-export const Footer = () => {
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+
+
+const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#logo" className='footer__logo'>JAKE</a>
+
+      <ul className='permalinks'>
+        <li><a href="#top">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#porfolio">Portfolio</a></li>
+        <li><a href="#testmonials">Testmonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/chuljungkim/"><FaFacebookF/></a>
+        <a href="https://www.instagram.com/a_jc.k/"><FiInstagram/></a>
+      </div>
+      
+      <div className="footer__copyright">
+        <small>&copy; Jake tutorials. All rights reserved</small>
+      </div>
+  
+    </footer>
   )
 }
+
+export default Footer
